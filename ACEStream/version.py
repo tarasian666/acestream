69c0f71789063209a6a8a437c43894fed7e09b3f
Taras <silent.h8@gmail.com>
2013-05-21 12:56:08 +0300

    modified:   ACEStream/version.py

--- ACEStream/version.py
+++ ACEStream/version.py
@@ -1,4 +1,4 @@
 #Embedded file name: ACEStream\version.pyo
-VERSION = '2.0.8.7'
-VERSION_REV = '2191'
-VERSION_DATE = '2013/03/28 18:36:41'
+VERSION = '2.0.8.11'
+VERSION_REV = '2233'
+VERSION_DATE = '2013/05/20 00:00:00'
